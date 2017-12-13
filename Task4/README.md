611a742a6dfc5273e74364a0d5051c910c34b514
Надежда Карацапова <amdnady@yandex.ru>
2017-12-13 22:46:08 +0300

    Обновила настройки по умолчанию

--- Task4/README.md
+++ Task4/README.md
@@ -12,9 +12,9 @@ sh bmp_encryption/bin/bmp_encryption console
 
 #### Query examples ####
 ```
-curl -v -H "Content-Type: application/octet-stream" -T image.bmp   localhost:8080?fileName=<file_name>
+curl -v -H "Content-Type: application/octet-stream" -T <source_file> localhost:8080?fileName=<file_name>
 
-curl -X GET -v  'localhost:8080?file=<file_name>&text=<text>' > <file_name>
+curl -X GET -v  localhost:8080?file=<file_name>&text=<text> > <file_name>
 
 curl -X DELETE -v localhost:8080?file=<file_name>
 
--- Task4/apps/bmp_encryption/src/bmp_encryption_app.erl
+++ Task4/apps/bmp_encryption/src/bmp_encryption_app.erl
@@ -7,19 +7,16 @@
 start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
         {'_', [
-        		{"/", cowboy_static, {priv_file, bmp_encryption_erl, "index.html"}},
-            {"/up", rest_bmp_encryption, []},
-            {"/[...]", bmp_encryption_handler, []}
-        		]
+                {"/", rest_bmp_encryption, []}
+                ]
         }
     ]),
-	{ok, _} = cowboy:start_clear(my_http_listener,
+    {ok, _} = cowboy:start_clear(my_http_listener,
         [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
     ),
-	bmp_encryption_sup:start_link().
+    bmp_encryption_sup:start_link().
     
 
 stop(_State) ->
-	ok.
-
+    ok.
